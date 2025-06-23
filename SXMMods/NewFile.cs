7d2a08996f0bf15bb6f32bc120f8d2dfe5572f5b
SXM5111226 <2540709491@users.noreply.github.com>
2025-06-23 11:53:14 +0800

    修复错误

--- SXMMods/NewFile.cs
+++
@@ -1,7 +0,0 @@
-private string FilterMP3Paths(string input)
-{
-    return string.Join("\n", input.Split('\n')
-        .Where(path => 
-            path.Trim().EndsWith(".mp3", StringComparison.OrdinalIgnoreCase))
-        .Distinct());
-}
